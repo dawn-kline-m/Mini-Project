0703d09bd73bd056a68df816bde7ff8e5c62f42f
jgum63 <joshgum3@gmail.com>
2023-04-14 13:21:13 -0500

    added fetch

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+fetch("https://www.loc.gov/search/?q=baseball&fo=json")
+.then(function(response){
+
+    return response.json()
+})
+.then(function(data){
+console.log(data)
+
+})
