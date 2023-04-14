d419fa1dfd625be670a8afacf15c7e0b1554fc57
Axel Estrada <axelestrada1890@gmail.com>
2023-04-14 13:49:25 -0700

    feature/table

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 fetch("https://www.loc.gov/search/?q=baseball&fo=json")
-.then(function(response){
+    .then(function (response) {
 
-    return response.json()
-})
-.then(function(data){
-console.log(data)
+        return response.json()
+    })
+    .then(function (data) {
 
-})
+        for (let i = 0; i < data.results.length; i++) {
+
+
+            let titleEl = document.createElement("p")
+            titleEl.textContent = data.results[i].title;
+
+            document.body.appendChild(titleEl);
+
+
+            let urlEl = document.createElement("a");
+            urlEl.textContent = data.results[i].url;
+            urlEl.href = data.results[i].url;
+
+            document.body.appendChild(titleEl);
+            document.body.appendChild(urlEl);
+
+        }
+
+    })
+
